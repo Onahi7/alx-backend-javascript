f24d5ddd05a65d530bd4ff28513687c56650f8c8
Onahi7 <dicksonhardy7@gmail.com>
2024-05-07 09:36:03 +0100

    hardy_java

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,16 +1,10 @@
-import { createUser, uploadPhoto } from './utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  // Call the uploadPhoto function to upload the user's photo then chain all
-  // promises together and handle with one error statement
   return Promise
     .all([uploadPhoto(), createUser()])
-    .then((userResp) => {
-      // If the user was successfully created, log the body, firstName, and lastName to the console
-      console.log(`${userResp[0].body} ${userResp[1].firstName} ${userResp[1].lastName}`);
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
     })
-    .catch(() => {
-      // In the event of an error, log Signup system offline to the console
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,3 @@
 export default function uploadPhoto(filename) {
-  return new Promise((resolve, reject) => {
-    // Reject the promise with an error and the formatted string
-    reject(new Error(`${filename} cannot be processed`));
-  });
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
